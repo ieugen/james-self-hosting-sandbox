1230916843851f2a9d8debbb80ea01884c67799d
David Leangen <david@leangen.net>
2020-07-29 21:08:23 +0900

    Mailet is now functional

--- james-mailets/src/main/java/net/leangen/james/mailet/sample/LogAndDoNothingMailet.java
+++ james-mailets/src/main/java/net/leangen/james/mailet/sample/LogAndDoNothingMailet.java
@@ -4,17 +4,21 @@ import javax.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.mailet.Mail;
 import org.apache.mailet.base.GenericMailet;
+import org.apache.mailet.base.GenericMatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Slf4j
+//@Slf4j
 public class LogAndDoNothingMailet extends GenericMailet {
+  private static final Logger LOGGER = LoggerFactory.getLogger(GenericMatcher.class);
 
   @Override
   public void init() throws MessagingException {
-    log.info("initialize mailet");
+    LOGGER.info("initialize Custom mailet");
   }
 
   @Override
   public void service(Mail mail) throws MessagingException {
-    log.info("Service the email {}", mail.getName());
+    LOGGER.info("Service the email {} using the Custom mailet", mail.getName());
   }
 }
--- james-mailets/src/main/java/net/leangen/james/mailet/sample/MatcherMadeInHeaven.java
+++ james-mailets/src/main/java/net/leangen/james/mailet/sample/MatcherMadeInHeaven.java
@@ -7,18 +7,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.james.core.MailAddress;
 import org.apache.mailet.Mail;
 import org.apache.mailet.base.GenericMatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Slf4j
+//@Slf4j
 public class MatcherMadeInHeaven extends GenericMatcher {
+  private static final Logger LOGGER = LoggerFactory.getLogger(GenericMatcher.class);
 
   @Override
   public void init() throws MessagingException {
-    log.info("Build Heaven. Initialize matcher");
+    LOGGER.info("Build Heaven. Initialize the Custom matcher");
   }
 
   @Override
   public Collection<MailAddress> match(Mail mail) throws MessagingException {
-    log.info("Matching for heaven {}", mail.getName());
+    LOGGER.info("Matching for heaven {} using the Customer matcher", mail.getName());
     return Collections.emptyList();
   }
 }
