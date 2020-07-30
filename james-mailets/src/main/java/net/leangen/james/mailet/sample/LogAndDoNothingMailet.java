e0e0cb223475606187eda6a0fa9597622ed184f5
David Leangen <david@leangen.net>
2020-07-31 07:00:25 +0900

    Reverted to use Lombok annotations for logging

--- james-mailets/src/main/java/net/leangen/james/mailet/sample/LogAndDoNothingMailet.java
+++ james-mailets/src/main/java/net/leangen/james/mailet/sample/LogAndDoNothingMailet.java
@@ -8,17 +8,16 @@ import org.apache.mailet.base.GenericMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//@Slf4j
+@Slf4j
 public class LogAndDoNothingMailet extends GenericMailet {
-  private static final Logger LOGGER = LoggerFactory.getLogger(GenericMatcher.class);
 
   @Override
   public void init() throws MessagingException {
-    LOGGER.info("initialize Custom mailet");
+    log.info("initialize Custom mailet");
   }
 
   @Override
   public void service(Mail mail) throws MessagingException {
-    LOGGER.info("Service the email {} using the Custom mailet", mail.getName());
+    log.info("Service the email {} using the Custom mailet", mail.getName());
   }
 }
--- james-mailets/src/main/java/net/leangen/james/mailet/sample/MatcherMadeInHeaven.java
+++ james-mailets/src/main/java/net/leangen/james/mailet/sample/MatcherMadeInHeaven.java
@@ -10,18 +10,17 @@ import org.apache.mailet.base.GenericMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//@Slf4j
+@Slf4j
 public class MatcherMadeInHeaven extends GenericMatcher {
-  private static final Logger LOGGER = LoggerFactory.getLogger(GenericMatcher.class);
 
   @Override
   public void init() throws MessagingException {
-    LOGGER.info("Build Heaven. Initialize the Custom matcher");
+    log.info("Build Heaven. Initialize the Custom matcher");
   }
 
   @Override
   public Collection<MailAddress> match(Mail mail) throws MessagingException {
-    LOGGER.info("Matching for heaven {} using the Customer matcher", mail.getName());
+    log.info("Matching for heaven {} using the Customer matcher", mail.getName());
     return Collections.emptyList();
   }
 }
